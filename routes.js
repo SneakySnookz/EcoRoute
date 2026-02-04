436d0faa7469f52f760ac95ed9e27544fe3bc559
Kaedyn Waters <kwaters@smail.pembinatrails.ca>
2026-02-04 11:36:15 -0600

    Added Extra Features

---
+++ routes.js
@@ -0,0 +1,20 @@
+window.routes = [
+    {"from": "JFK", "to": "LAX", "co2": 500},
+    {"from": "JFK", "to": "ORD", "co2": 200},
+    {"from": "ORD", "to": "LAX", "co2": 300},
+    {"from": "JFK", "to": "ATL", "co2": 250},
+    {"from": "ATL", "to": "LAX", "co2": 400},
+    {"from": "ATL", "to": "DFW", "co2": 150},
+    {"from": "DFW", "to": "LAX", "co2": 200},
+    {"from": "ORD", "to": "DFW", "co2": 180},
+    {"from": "DFW", "to": "SFO", "co2": 250},
+    {"from": "SFO", "to": "LAX", "co2": 100},
+    {"from": "JFK", "to": "SFO", "co2": 550},
+    {"from": "SFO", "to": "SEA", "co2": 120},
+    {"from": "SEA", "to": "LAX", "co2": 200},
+    {"from": "WIN", "to": "LAX", "co2": 300}, // THIS LINE AND FURTHER IS MINE
+    {"from": "WIN", "to": "SEA", "co2": 220},
+    {"from": "WIN", "to": "ORD", "co2": 400},
+    {"from": "ORD", "to": "WIN", "co2": 500},
+    {"from": "LAX", "to": "WIN", "co2": 324}
+]
