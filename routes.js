ccf3b2e57eea7ac9199cedfa0c6d3154ec39048a
Kaedyn Waters <kwaters@smail.pembinatrails.ca>
2026-02-04 11:36:15 -0600

    Added Extra Features

--- routes.js
+++ routes.js
@@ -11,5 +11,10 @@ window.routes = [
     {"from": "SFO", "to": "LAX", "co2": 100},
     {"from": "JFK", "to": "SFO", "co2": 550},
     {"from": "SFO", "to": "SEA", "co2": 120},
-    {"from": "SEA", "to": "LAX", "co2": 200}
+    {"from": "SEA", "to": "LAX", "co2": 200},
+    {"from": "WIN", "to": "LAX", "co2": 300}, // THIS LINE AND FURTHER IS MINE
+    {"from": "WIN", "to": "SEA", "co2": 220},
+    {"from": "WIN", "to": "ORD", "co2": 400},
+    {"from": "ORD", "to": "WIN", "co2": 500},
+    {"from": "LAX", "to": "WIN", "co2": 324}
 ]
